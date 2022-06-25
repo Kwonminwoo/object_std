84eefffc678ec20a8e8730a5f72670eab7180a08
KwonMinwoo <34360434+Kwonminwoo@users.noreply.github.com>
2022-06-25 22:05:57 +0900

    add java file c1d1

---
+++ chapter1/day1/TicketOffice.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TicketOffice {
+    private Long amount;
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public TicketOffice (Long amount, Ticket ... tickets){
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+
+    public Ticket getTicket(){
+        return tickets.remove(0); // 티켓을 교환 및 판매하면 티켓 묶음 맨 위의 티켓을 판매한다고 가정.
+    }
+
+    public void minusAmount(Long amount){
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount){
+        this.amount += amount;
+    }
+}
